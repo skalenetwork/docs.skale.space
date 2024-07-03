dd4b2467af01107621f89f0ae2f8f1e137f8d2b0
TheGreatAxios <sawyer@dirtroad.dev>
2024-07-03 12:39:59 -0700

    Migrate SKALE Chain Wallet

--- astro.config.mjs
+++ astro.config.mjs
@@ -45,7 +45,8 @@ export default defineConfig({
 								{ label: "Overview", link: "/builders/chain-operators/overview" },
 								{ label: "Architecture", link: "/builders/chain-operators/architecture" },
 								{ label: "Access Control", link: "/builders/chain-operators/access-control" },
-								{ label: "Best Practices", link: "/builders/chain-operators/best-practices" },
+								// TODO
+								// { label: "Best Practices", link: "/builders/chain-operators/best-practices" },
 								{ label: "Administrative Contracts", link: "/builders/chain-operators/administrative-contracts" },
 								{ label: "Manage Multisig", link: "/builders/chain-operators/multisig" },
 								{ label: "SKALE Chain Wallet", link: "/builders/chain-operators/skale-chain-wallet" },
