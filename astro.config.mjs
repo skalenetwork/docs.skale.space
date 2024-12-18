486579629a7c3e241e9a44b328ad812544e44331
manuelbarbas <manuel@skalelabs.com>
2024-12-18 18:03:57 +0000

    Fix banner

--- astro.config.mjs
+++ astro.config.mjs
@@ -27,6 +27,8 @@ export default defineConfig({
 				Sidebar: "./src/components/Overrides/Sidebar.astro",
 				SidebarSublist:
 					"./src/components/Overrides/SidebarSublist.astro",
+				MobileMenuToggle: "./src/components/Overrides/MobileMenuToggle.astro",
+				MobileTableOfContents: "./src/components/Overrides/MobileTableOfContents.astro",
 			},
 			expressiveCode: true,
 			favicon: "/favicon.png",
