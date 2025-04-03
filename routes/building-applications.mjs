509d78e261132e0c80dd973c802e54843bebb0f8
TheGreatAxios <sawyer@dirtroad.dev>
2025-04-02 18:52:39 -0700

    Add Using Filestroage, How Filstorage Works, and Base Upgradeable Contracts

--- routes/building-applications.mjs
+++ routes/building-applications.mjs
@@ -4,7 +4,6 @@ export default [
     { "label": "Gasless Transactions", "slug": "building-applications/gasless-transactions" },
     { "label": "Payments on SKALE", "slug": "building-applications/payments-on-skale" },
     { "label": "Using Contract Factories", "slug": "building-applications/using-contract-factories" },
-    { "label": "Native Filestorage", "slug": "building-applications/native-filestorage" },
     { "label": "Random Number Generation", "slug": "building-applications/random-number-generation" },
     { "label": "SKALE vs Ethereum", "slug": "building-applications/skale-vs-ethereum" },
     { "label": "Solidity Quickstart", "slug": "building-applications/solidity-quickstart" },
@@ -12,5 +11,6 @@ export default [
     { "label": "Wallet Integration", "slug": "building-applications/wallet-integration" },
     { "label": "JSON-RPC Interface", "slug": "building-applications/json-rpc-interface" },
     { "label": "Pick a chain", "slug": "building-applications/pick-a-chain" },
+    { "label": "Using Filestorage", "slug": "building-applications/using-filestorage" },
     { "label": "FAQs", "slug": "building-applications/faqs" }
 ]
--- routes/how-skale-works.mjs
+++ routes/how-skale-works.mjs
@@ -3,6 +3,7 @@ export default [
     { "label": "Bridge Architecture", "slug": "how-skale-works/bridge-architecture" },
     { "label": "Permission Layer", "slug": "how-skale-works/permission-layer" },
     { "label": "Delegation Model", "slug": "how-skale-works/delegation-model" },
+    { "label": "SKALE Filestorage", "slug": "how-skale-works/filestorage" },
     { "label": "Staking Lifecycle", "slug": "how-skale-works/staking-lifecycle" },
     { "label": "Inheriting Security", "slug": "how-skale-works/inheriting-security" },
     { "label": "EVM Compatibility", "slug": "how-skale-works/evm-compatibility" },
