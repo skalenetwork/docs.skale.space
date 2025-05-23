7e1e027a2fc865ba592ae417fa62d57a86425995
TheGreatAxios <sawyer@dirtroad.dev>
2025-05-23 16:50:45 -0700

    Begin Run a SKALE Chain Docs

--- routes/run-a-skale-chain.mjs
+++ routes/run-a-skale-chain.mjs
@@ -1,19 +1,58 @@
 export default [
-    { "label": "Intro to sChains", "slug": "run-a-skale-chain/intro-to-schains" },
-    { "label": "Setup Requirements", "slug": "run-a-skale-chain/setup-requirements" },
-    { "label": "Managing State Consumption", "slug": "run-a-skale-chain/managing-state-consumption" },
-    { "label": "Managing Deployment Permissions", "slug": "run-a-skale-chain/managing-deployment-permissions" },
-    { "label": "Managing sFUEL Distribution", "slug": "run-a-skale-chain/managing-sfuel-distribution" },
-    { "label": "Connecting sChains", "slug": "run-a-skale-chain/connecting-schains" },
-    { "label": "Add Bridgeable Token", "slug": "run-a-skale-chain/add-bridgeable-token" },
-    { "label": "sFUEL Bridge Top-ups", "slug": "run-a-skale-chain/sfuel-bridge-topups" },
-    { "label": "sChain Ownership Deep Dive", "slug": "run-a-skale-chain/schain-ownership-deep-dive" },
-    { "label": "Using Multisigwallet CLI", "slug": "run-a-skale-chain/using-multisig-wallet-cli" },
-    { "label": "sChain Operations", "slug": "run-a-skale-chain/schain-operations" },
-    { "label": "Customize sChain", "slug": "run-a-skale-chain/customize-schain" },
-    { "label": "sChain Infrastructure", "slug": "run-a-skale-chain/schain-infrastructure" },
-    { "label": "Implement Transaction Fees", "slug": "run-a-skale-chain/implement-transaction-fees" },
-    { "label": "Pricing and Payments", "slug": "run-a-skale-chain/pricing-and-payments" },
-    { "label": "Upgradeable Contracts", "slug": "run-a-skale-chain/upgradeable-contracts" },
-    { "label": "FAQ", "slug": "run-a-skale-chain/faq" }
-]
+	{ label: "Intro to sChains", slug: "run-a-skale-chain/intro-to-schains" },
+	{
+		label: "Setup Requirements",
+		slug: "run-a-skale-chain/setup-requirements",
+	},
+	{ label: "Customize sChain", slug: "run-a-skale-chain/customize-schain" },
+	{
+		label: "Managing State Consumption",
+		slug: "run-a-skale-chain/managing-state-consumption",
+	},
+	{
+		label: "Managing Deployment Permissions",
+		slug: "run-a-skale-chain/managing-deployment-permissions",
+	},
+	{
+		label: "Managing sFUEL Distribution",
+		slug: "run-a-skale-chain/managing-sfuel-distribution",
+	},
+	{
+		label: "Connecting sChains",
+		slug: "run-a-skale-chain/connecting-schains",
+	},
+	{
+		label: "Add Bridgeable Token",
+		slug: "run-a-skale-chain/add-bridgeable-token",
+	},
+	{
+		label: "sFUEL Bridge Top-ups",
+		slug: "run-a-skale-chain/sfuel-bridge-topups",
+	},
+	{
+		label: "sChain Ownership Deep Dive",
+		slug: "run-a-skale-chain/schain-ownership-deep-dive",
+	},
+	{
+		label: "Using Multisigwallet CLI",
+		slug: "run-a-skale-chain/using-multisig-wallet-cli",
+	},
+	{ label: "sChain Operations", slug: "run-a-skale-chain/schain-operations" },
+	{
+		label: "sChain Infrastructure",
+		slug: "run-a-skale-chain/schain-infrastructure",
+	},
+	{
+		label: "Implement Transaction Fees",
+		slug: "run-a-skale-chain/implement-transaction-fees",
+	},
+	{
+		label: "Pricing and Payments",
+		slug: "run-a-skale-chain/pricing-and-payments",
+	},
+	{
+		label: "Upgradeable Contracts",
+		slug: "run-a-skale-chain/upgradeable-contracts",
+	},
+	{ label: "FAQ", slug: "run-a-skale-chain/faq" },
+];
