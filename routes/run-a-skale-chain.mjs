503a7770cf17a9120464219a1256f0c8b5ba8af3
TheGreatAxios <sawyer@dirtroad.dev>
2025-05-26 15:32:14 -0700

    Remove Future Pages

--- routes/run-a-skale-chain.mjs
+++ routes/run-a-skale-chain.mjs
@@ -1,5 +1,8 @@
 export default [
-	{ label: "Intro to sChains", slug: "run-a-skale-chain/intro-to-schains" },
+	{
+		label: "Intro to sChains",
+		slug: "run-a-skale-chain/intro-to-schains"
+	},
 	{
 		label: "Setup Requirements",
 		slug: "run-a-skale-chain/setup-requirements",
@@ -17,22 +20,6 @@ export default [
 		slug: "run-a-skale-chain/managing-sfuel-distribution",
 	},
 	{
-		label: "Managing State Consumption",
-		slug: "run-a-skale-chain/managing-state-consumption",
-	},
-	{
-		label: "Connecting sChains",
-		slug: "run-a-skale-chain/connecting-schains",
-	},
-	{
-		label: "Add Bridgeable Token",
-		slug: "run-a-skale-chain/add-bridgeable-token",
-	},
-	{
-		label: "sFUEL Bridge Top-ups",
-		slug: "run-a-skale-chain/sfuel-bridge-topups",
-	},
-	{
 		label: "sChain Ownership Deep Dive",
 		slug: "run-a-skale-chain/schain-ownership-deep-dive",
 	},
@@ -40,22 +27,8 @@ export default [
 		label: "Using Multisigwallet CLI",
 		slug: "run-a-skale-chain/using-multisig-wallet-cli",
 	},
-	{ label: "sChain Operations", slug: "run-a-skale-chain/schain-operations" },
-	{
-		label: "sChain Infrastructure",
-		slug: "run-a-skale-chain/schain-infrastructure",
-	},
-	{
-		label: "Implement Transaction Fees",
-		slug: "run-a-skale-chain/implement-transaction-fees",
-	},
 	{
 		label: "Pricing and Payments",
 		slug: "run-a-skale-chain/pricing-and-payments",
-	},
-	{
-		label: "Upgradeable Contracts",
-		slug: "run-a-skale-chain/upgradeable-contracts",
-	},
-	{ label: "FAQ", slug: "run-a-skale-chain/faq" },
+	}
 ];
