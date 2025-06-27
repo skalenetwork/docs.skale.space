03bf9a2ca9bbdc3269e369e2820d7827726da718
TheGreatAxios <sawyer@dirtroad.dev>
2025-06-27 16:46:48 -0700

    Reorganize Bridge Dir, Add Landing Page & MessageProxy Doc

--- routes/skale-bridge.mjs
+++ routes/skale-bridge.mjs
@@ -1,16 +1,39 @@
 export default [
-    { "label": "Intro to Bridging", "slug": "skale-bridge/intro-to-bridging" },
-    { "label": "Create Custom Contracts", "slug": "skale-bridge/create-custom-contracts" },
-    { "label": "Connect Custom Contracts", "slug": "skale-bridge/connect-custom-contracts" },
-    { "label": "Bridge ETH", "slug": "skale-bridge/bridge-eth" },
-    { "label": "Bridge ERC-20", "slug": "skale-bridge/bridge-erc20" },
-    { "label": "Bridge ERC-721", "slug": "skale-bridge/bridge-erc721" },
-    { "label": "Bridge ERC-1155", "slug": "skale-bridge/bridge-erc1155" },
-    { "label": "Exit to Ethereum", "slug": "skale-bridge/exit-to-ethereum" },
-    { "label": "IMA Limits", "slug": "skale-bridge/ima-limits" },
+    { "label": "Overview", "slug": "skale-bridge/overview" },
+    {
+        "label": "Messaging Layer",
+        "items": [
+            { "label": "Message Proxy", "slug": "skale-bridge/messaging-layer/message-proxy" },
+            { "label": "Create Custom Contracts", "slug": "skale-bridge/messaging-layer/create-custom-contracts" },
+            { "label": "Connect Custom Contracts", "slug": "skale-bridge/messaging-layer/connect-custom-contracts" },
+            { "label": "Using Custom Contracts", "slug": "skale-bridge/messaging-layer/using-custom-contracts" }
+        ],
+        "collapsed": true
+    },
+    {
+        "label": "Ethereum Bridge",
+        "items": [
+            { "label": "Bridge ETH", "slug": "skale-bridge/ethereum/bridge-eth" },
+            { "label": "Bridge ERC-20", "slug": "skale-bridge/ethereum/bridge-erc20" },
+            { "label": "Bridge ERC-721", "slug": "skale-bridge/ethereum/bridge-erc721" },
+            { "label": "Bridge ERC-1155", "slug": "skale-bridge/ethereum/bridge-erc1155" },
+            { "label": "Exit to Ethereum", "slug": "skale-bridge/ethereum/exit-to-ethereum" },
+        ],
+        "collapsed": true
+    },
+    {
+        "label": "SKALE Chain Bridge",
+        "items": [
+            { "label": "Overview", "slug": "skale-bridge/skale/overview" },
+            { "label": "Bridge ERC-20", "slug": "skale-bridge/skale/bridge-erc20" },
+            { "label": "Bridge ERC-721", "slug": "skale-bridge/skale/bridge-erc721" },
+            { "label": "Bridge ERC-1155", "slug": "skale-bridge/skale/bridge-erc1155" },
+        ],
+        "collapsed": true
+    },
+    { "label": "Architecture of IMA", "slug": "skale-bridge/architecture-of-ima" },
+    { "label": "Interchain Tokens", "slug": "skale-bridge/interchain-tokens" },
     { "label": "Security Methods", "slug": "skale-bridge/security-methods" },
     { "label": "Token Availability", "slug": "skale-bridge/token-availability" },
-    { "label": "Token Flows", "slug": "skale-bridge/token-flows" },
-    { "label": "TokenManager vs DepositBox", "slug": "skale-bridge/token-manager-vs-deposit-box" },
-    { "label": "Troubleshooting", "slug": "skale-bridge/troubleshooting" }
+    
 ]
