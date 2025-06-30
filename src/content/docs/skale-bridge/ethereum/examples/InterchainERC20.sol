ef6baa9c9894b56e8ee7a5397af84cd36c571432
TheGreatAxios <sawyer@dirtroad.dev>
2025-06-29 17:50:18 -0700

    Begin ERC-721 Bridging S2E

--- src/content/docs/skale-bridge/ethereum/examples/InterchainERC20.sol
+++ src/content/docs/skale-bridge/ethereum/examples/InterchainERC20.sol
@@ -15,11 +15,6 @@ contract InterchainERC20 is ERC20, AccessControl {
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
     bytes32 public constant BURNER_ROLE = keccak256("BURNER_ROLE");
 
-    /**
-     * @notice Constructor initializes the ERC20 token and sets up roles.
-     * @param name The name of the token.
-     * @param symbol The symbol of the token.
-     */
     constructor(string memory name, string memory symbol) ERC20(name, symbol) {
         // Assign the minter role to a predefined address.
         _grantRole(MINTER_ROLE, 0xD2aAA00500000000000000000000000000000000);
@@ -28,15 +23,6 @@ contract InterchainERC20 is ERC20, AccessControl {
         _grantRole(BURNER_ROLE, 0xD2aAA00500000000000000000000000000000000);
     }
 
-    /**
-     * @notice Mints new tokens and assigns them to the specified address.
-     * @dev Caller must have the MINTER_ROLE to invoke this function.
-     * @param to The address that will receive the newly minted tokens.
-     * @param amount The amount of tokens to mint.
-     * Requirements:
-     * - Caller must have MINTER_ROLE.
-     * - `to` cannot be the zero address.
-     */
     function mint(address to, uint256 amount) public virtual {
         // Ensure that the caller has the MINTER_ROLE.
         require(hasRole(MINTER_ROLE, msg.sender), "Caller is not a minter");
@@ -45,14 +31,6 @@ contract InterchainERC20 is ERC20, AccessControl {
         _mint(to, amount);
     }
 
-    /**
-     * @notice Burns tokens from the caller's account.
-     * @dev Caller must have the BURNER_ROLE to invoke this function.
-     * @param amount The amount of tokens to burn.
-     * Requirements:
-     * - Caller must have BURNER_ROLE.
-     * - Caller must have at least `amount` tokens.
-     */
     function burn(uint256 amount) public virtual {
         // Ensure that the caller has the BURNER_ROLE.
         require(hasRole(BURNER_ROLE, msg.sender), "Caller is not a burner");
