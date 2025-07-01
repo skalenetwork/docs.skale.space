b8a5a87dd2b0a309eea3fc05085e85cda16e3439
TheGreatAxios <sawyer@dirtroad.dev>
2025-06-30 21:29:14 -0700

    Add llms.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,29 @@ import routes from "./routes/index.mjs";
 import react from "@astrojs/react";
 import starlightLinksValidator from "starlight-links-validator";
 import sitemap from "@astrojs/sitemap";
+import starlightLlmsTxt from "starlight-llms-txt";
+
+const llms = starlightLlmsTxt({
+	projectName: "SKALE Network",
+	description: "SKALE is a network of layer 1 blockchains offering infinite scalability, zero gas fees, and one of the fastest EVMs in thew world.",
+	optionalLinks: [
+		{
+			label: "SKALE Network Github",
+			url: "https://github.com/skalenetwork",
+			description: "SKALE Network GitHub Organization"
+		},
+		{
+			label: "Dirt Road Dev Docs",
+			url: "https://docs.dirtroad.dev",
+			description: "3rd party contributor Dirt Road Development docs which contains key SKALE contributions"
+		},
+		{
+			label: "Eidolon Docs",
+			url: "https://docs.eidolon.gg",
+			description: "3rd party contributor Eidolon Labs docs which contains key SKALE contributions"
+		}
+	]
+})
 
 // https://astro.build/config
 export default defineConfig({
@@ -30,7 +53,12 @@ export default defineConfig({
 			// If you remove and it breaks, I will be sad :(
 			// @ts-ignore
 			sidebar: routes,
-			plugins: process.env.CHECK_LINKS ?  [starlightLinksValidator()] : []
+			plugins: process.env.CHECK_LINKS ?  [
+				starlightLinksValidator(),
+				llms,
+			] : [
+				llms
+			]
 		})
 	],
 });
