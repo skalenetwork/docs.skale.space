5029508fbe2415be2fcf0c3cf37db353f60fba4a
TheGreatAxios <sawyer@dirtroad.dev>
2025-06-30 22:15:29 -0700

    Minor reorganization, spelling fixes, small tweaks based on mbarbas recs

--- routes/building-applications.mjs
+++ routes/building-applications.mjs
@@ -1,7 +1,6 @@
 export default [
     { "label": "Building a dApp", "slug": "building-applications/build-a-dapp" },    
-    { "label": "Zero Gas Fees", "slug": "building-applications/zero-gas-fees" },
-    { "label": "Gasless Transactions", "slug": "building-applications/gasless-transactions" },
+    { "label": "Gas Fees", "slug": "building-applications/gas-fees" },
     { "label": "Payments on SKALE", "slug": "building-applications/payments-on-skale" },
     { "label": "Using Contract Factories", "slug": "building-applications/using-contract-factories" },
     { "label": "Random Number Generation", "slug": "building-applications/random-number-generation" },
@@ -9,11 +8,11 @@ export default [
     { "label": "Solidity Quickstart", "slug": "building-applications/solidity-quickstart" },
     { "label": "JSON-RPC Interface", "slug": "building-applications/json-rpc-interface" },
     { "label": "Pick a chain", "slug": "building-applications/pick-a-chain" },
-    { "label": "Using Filestorage", "slug": "building-applications/using-filestorage" },
     { "label": "Troubleshooting", "slug": "building-applications/troubleshooting" },
     {
-        "label": "Libraries",
+        "label": "Filestorage",
         "items": [
+            {"label": "Using Filestorage", "slug": "building-applications/using-filestorage" },
             { "label": "Filestorage.js", "slug": "building-applications/libraries/filestorage-js", "collapsed": true },
         ]
     }
--- routes/welcome.mjs
+++ routes/welcome.mjs
@@ -1,5 +1,5 @@
 export default [
     { "label": "Intro to SKALE", "slug": "welcome/intro-to-skale" },
     { "label": "Get started", "slug": "welcome/get-started" },
-    { "label": "Chain info", "slug": "welcome/chain-info" }
+    { "label": "sChains Info", "slug": "welcome/chain-info" }
 ]
