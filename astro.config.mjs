58e5bec668dff8ca49eca3c66c4387aa2a75bb71
TheGreatAxios <sawyer@dirtroad.dev>
2025-10-22 21:05:26 -0700

    Remove 3rd party docs

--- astro.config.mjs
+++ astro.config.mjs
@@ -16,18 +16,7 @@ const llms = starlightLlmsTxt({
 			label: "SKALE Network Github",
 			url: "https://github.com/skalenetwork",
 			description: "SKALE Network GitHub Organization"
-		},
-		{
-			label: "Dirt Road Dev Docs",
-			url: "https://docs.dirtroad.dev",
-			description: "3rd party contributor Dirt Road Development docs which contains key SKALE contributions"
-		},
-		{
-			label: "Eidolon Docs",
-			url: "https://docs.eidolon.gg",
-			description: "3rd party contributor Eidolon Labs docs which contains key SKALE contributions"
-		}
-	]
+		}	]
 })
 
 // https://astro.build/config
