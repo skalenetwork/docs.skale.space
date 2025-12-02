c70951b3403d0993e2a7db3c143b22a71fcd41cf
TheGreatAxios <sawyer@dirtroad.dev>
2025-12-01 17:59:26 -0800

    Remove Old v2 Docs

--- old-stuff/redirects/index.mjs
+++
@@ -1,8 +0,0 @@
- import networkDocsRedirects from "./networkDocsRedirects.mjs";
-import spaceDocsRedirects from "./spaceDocsRedirects.mjs";
-
-export default {
-	...networkDocsRedirects,
-	...spaceDocsRedirects,
-	"/": "/welcome/get-started"
-};
